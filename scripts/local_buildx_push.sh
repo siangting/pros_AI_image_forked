c6e8ff568e96e092b910ae9a1b7f6bc4535b07f1
otischung <my072814638@gmail.com>
2024-05-23 10:44:55 +0800

    [FEAT] Add tag argument into local buildx push

--- scripts/local_buildx_push.sh
+++ scripts/local_buildx_push.sh
@@ -1,8 +1,37 @@
 export IMG_NAME="pros_ai_image"
 export ECR_URL="ghcr.io/otischung"
-export TODAY=$(date +%Y%m%d)
+export TAG=$(date +%Y%m%d)
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
+# Function to display a usage message
+usage() {
+    echo "Usage: $0 -t <tag>"
+    exit 0
+}
+
+# Parse command-line options
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -t)
+            TAG="$2"
+            shift 2
+            ;;
+        -h)
+            usage
+            shift
+            exit 0
+            ;;
+        --help)
+            usage
+            exit 0
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+echo "Using tag: $TAG"
 docker run --rm --privileged tonistiigi/binfmt:latest
 docker run --privileged --rm tonistiigi/binfmt --uninstall qemu-*
 docker run --privileged --rm tonistiigi/binfmt --install all
@@ -10,5 +39,5 @@ docker buildx create --use --platform=linux/arm64,linux/amd64 --name multi-platf
 docker buildx inspect --bootstrap
 docker buildx build --platform=linux/arm64,linux/amd64 --push \
     --tag $ECR_URL/$IMG_NAME:latest \
-    --tag $ECR_URL/$IMG_NAME:$TODAY \
+    --tag $ECR_URL/$IMG_NAME:$TAG \
     -f ./Dockerfile .
