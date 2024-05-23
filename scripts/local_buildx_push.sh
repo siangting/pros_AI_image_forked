0ec3298601ae34a8a6ffdc5d9684c8b33d43193c
otischung <my072814638@gmail.com>
2024-05-23 11:00:48 +0800

    [FIX] Remove latest tag from cuda branch

--- scripts/local_buildx_push.sh
+++ scripts/local_buildx_push.sh
@@ -38,6 +38,6 @@ docker run --privileged --rm tonistiigi/binfmt --install all
 docker buildx create --use --platform=linux/arm64,linux/amd64 --name multi-platform-builder
 docker buildx inspect --bootstrap
 docker buildx build --platform=linux/arm64,linux/amd64 --push \
-    --tag $ECR_URL/$IMG_NAME:latest \
+    # --tag $ECR_URL/$IMG_NAME:latest \
     --tag $ECR_URL/$IMG_NAME:$TAG \
     -f ./Dockerfile .
