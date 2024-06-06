76435002023e43b43a6e5dad4d9e13c645c5a623
otischung <my072814638@gmail.com>
2024-06-06 10:07:35 +0800

    [FEAT] Add --no-cache to buildx

--- scripts/local_buildx_push.sh
+++ scripts/local_buildx_push.sh
@@ -39,6 +39,6 @@ docker run --privileged --rm tonistiigi/binfmt --uninstall qemu-*
 docker run --privileged --rm tonistiigi/binfmt --install all
 docker buildx create --use --platform=linux/arm64,linux/amd64 --name multi-platform-builder
 docker buildx inspect --bootstrap
-docker buildx build --platform=linux/arm64,linux/amd64 --push \
+docker buildx build --no-cache --platform=linux/arm64,linux/amd64 --push \
     --tag $ECR_URL/$IMG_NAME:$TAG \
     -f ./Dockerfile .
